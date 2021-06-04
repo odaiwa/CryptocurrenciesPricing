7726c76f8cb0c9db72b5d757b6dd6314aa40277a
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2021-06-04 12:19:05 +0300

    save and retrieve from LocalStorage
    save data about crypto (price in three currencies nis, usd and euro.

--- main.js
+++ main.js
@@ -33,58 +33,109 @@ function displayCurrencies(Currencies) {
     }
 }
 
-function footerData(){
+function footerData() {
     const d = new Date();
     const n = d.getFullYear();
-    const footerinfo=`Copyright © ${n} Odai Wattad`
+    const footerinfo = `Copyright © ${n} Odai Wattad`
     document.getElementById("footer-p").innerHTML = footerinfo;
 }
 
 async function MoreInfoForCurrency(event) {
 
-
     const currencyMoreInfo = `more-info-about-currency-${event}`;
     const moreInfoAboutCurrency = document.getElementById(currencyMoreInfo);
     console.log(moreInfoAboutCurrency.innerText)
-    if (moreInfoAboutCurrency.innerText.length !== 0) {
-        $("#"+currencyMoreInfo).empty();
-    }
-    else {
-        console.log("there's no more info about currency")
-        try {
-            const response = await getJSON(`https://api.coingecko.com/api/v3/coins/${event}`);
-            $(currencyMoreInfo).empty();
+
+    let currentCurrency = [];
+    const jsonArray = localStorage.getItem(event);
+    if (jsonArray) {
+        currentCurrency = JSON.parse(jsonArray);
+        if (moreInfoAboutCurrency.innerText.length !== 0) {
+            $("#" + currencyMoreInfo).empty();
+        }
+        else{
+            console.log("toke from localStorage");
             const p = `
             <div class="currency-prices">
-                <p id="more-info-price">${response.market_data.current_price.ils} ₪</p>
-                <p id="more-info-price">${response.market_data.current_price.usd} $</p>
-                <p id="more-info-price">${response.market_data.current_price.eur} €</p>
+            <p id="more-info-price">${currentCurrency.nis} ₪</p>
+            <p id="more-info-price">${currentCurrency.usd} $</p>
+            <p id="more-info-price">${currentCurrency.euro} €</p>
             </div>
             `;
-            $("#"+currencyMoreInfo).append(p);
-
+            
+            $("#" + currencyMoreInfo).append(p);
         }
-        catch (err) {
-            alert(err.status);
+
+    }
+    else {
+
+        if (moreInfoAboutCurrency.innerText.length !== 0) {
+            $("#" + currencyMoreInfo).empty();
+        }
+        else {
+            console.log("from ajax")
+            // console.log("there's no more info about currency")
+            try {
+                const response = await getJSON(`https://api.coingecko.com/api/v3/coins/${event}`);
+                $(currencyMoreInfo).empty();
+
+                const currencyPrice = {
+                    id: event,
+                    nis: response.market_data.current_price.ils,
+                    usd: response.market_data.current_price.usd,
+                    euro: response.market_data.current_price.eur
+                }
+
+                console.log(currencyPrice)
+                const p = `
+                <div class="currency-prices">
+                <p id="more-info-price">${currencyPrice.nis} ₪</p>
+                <p id="more-info-price">${currencyPrice.usd} $</p>
+                <p id="more-info-price">${currencyPrice.euro} €</p>
+                </div>
+                `;
+                $("#" + currencyMoreInfo).append(p);
+
+                saveToLocalStorage(currencyPrice);
+            }
+            catch (err) {
+                alert(err.status);
+                console.log("Error Occured")
+            }
         }
     }
 }
+
+
+function saveToLocalStorage(data) {
+
+    const timer = new Date().getTime();
+    const currencyInfo = {
+        id: data.id,
+        nis: data.nis,
+        usd: data.usd,
+        euro: data.euro,
+        addedTime: timer
+    }
+    localStorage.setItem(data.id, JSON.stringify(currencyInfo));
+}
+
 const mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 
